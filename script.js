b7e79f9d4dd4dd6b61ad2172060ddc43cc230e06
Felipe Baz <fbazmitsuishi@gmail.com>
2021-08-19 19:00:51 -0300

    :up: Atualização dos textos

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+function saibaMaisIntro() {
+    window.open("https://github.com/Felipe-Baz");
+}

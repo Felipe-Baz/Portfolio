755be8714beadac1e77206c14fe40636ccaba7a1
Felipe Baz <fbazmitsuishi@gmail.com>
2021-09-29 03:14:55 -0300

    :package: Cálculo de Orçamento

--- script.js
+++ script.js
@@ -3,21 +3,41 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
 );
 
 function saibaMaisIntro() {
-    window.open("https://github.com/Felipe-Baz");
+    window.open("https://github.com/Felipe-Baz")
 }
 
 function saibaMaisProj1() {
-    window.open("https://github.com/Felipe-Baz/WineClassification");
+    window.open("https://github.com/Felipe-Baz/WineClassification")
 }
 
 function saibaMaisProj2() {
-    window.open("https://github.com/Felipe-Baz/PacMan");
+    window.open("https://github.com/Felipe-Baz/PacMan")
 }
 
 function saibaMaisProj3() {
-    window.open("https://github.com/Felipe-Baz/BreastCancer");
+    window.open("https://github.com/Felipe-Baz/BreastCancer")
 }
 
-function enviar() {
-    window.open('mailto:fbazmitsuishi@gmail.com?subject='+ +'&body='+ )
+document.querySelector("#qtde").addEventListener("change", atualizarpreco)
+document.querySelector("#js").addEventListener("change", atualizarpreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarpreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarpreco)
+document.querySelector("#prazo").addEventListener("change", function (){
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas.`
+    atualizarpreco()
+})
+
+function atualizarpreco() {
+    const qtde = document.querySelector("#qtde").value
+    const temJs = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+    let preco = qtde * 100;
+    if (temJs) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo * 0.1;
+    preco *= 1 + taxaUrgencia
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
 }
+
